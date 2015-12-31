5420397b69bce231c75a55dd18fe0a79e8f7a261
VamshiRakela <vamshi.rakela@outlook.com>
2015-12-31 19:57:55 +0530

    Create CB.java

---
+++ CB.java
@@ -0,0 +1,15 @@
+# LoopExample
+class CB 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=11110;i++)
+		{
+			for(int j=1;j<=108;j++)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
